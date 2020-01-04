2bd1094c4229af14afbd4b152f4bd062dd10e6f2
Konyahin Vitaliy <56167643+WgHost-git@users.noreply.github.com>
2020-01-04 16:59:02 +0300

    Update DetectFaceOpenCv.cpp

--- DetectFaceOpenCv.cpp
+++ DetectFaceOpenCv.cpp
@@ -1,31 +1,3 @@
-#include "opencv2/imgproc/imgproc.hpp"
-#include <iostream>
-#include <stdio.h>
-using namespace std;
-using namespace cv;
-
-int main()
-{
-    Mat image;
-    image = imread("d:/1.jpg", IMREAD_COLOR); // картинка
-
-    //resize(image, image, cv::Size(), 0.30, 0.30); // изменение размера картинки 0.30 - это 30% от размера картинки исходной
-
-    namedWindow("window1", 1); 
-    imshow("window1", image);
-
-    // Load Face cascade (.xml file) (Нагрузочный Каскад граней (.XML-файл))
-    CascadeClassifier face_cascade;
-    face_cascade.load("haarcascade_frontalface_alt2.xml");  // Надо скопировать из opencv\build\etc\haarcascades\ в папку с проектом!!!
-
-    if (!face_cascade.load("haarcascade_frontalface_alt2.xml")) { cout << "--(!)Error loading face cascade\n"; return -1; }; // Проверка загрузки каскада
-    //if (!eyes_cascade.load(eyes_cascade_name)) { printf("--(!)Error loading eyes cascade\n"); return -1; };
-
-    // Детект лица
-    vector<Rect> faces;
-    face_cascade.detectMultiScale(image, faces, 1.1, 2, 0 | CASCADE_SCALE_IMAGE, Size(20, 30));
-
-titled 
 #include "opencv2/objdetect/objdetect.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
@@ -33,21 +5,22 @@ titled
 #include <stdio.h>
 using namespace std;
 using namespace cv;
-​
+
 int main()
 {
     Mat image = imread("d:/1.jpg", IMREAD_COLOR); // картинка
-​
+
     //resize(image, image, cv::Size(), 0.30, 0.30); // изменение размера картинки 0.30 - это 30% от размера картинки исходной
-​
+
     namedWindow("window1", 1); 
     imshow("window1", image);
-​
+
     // Load Face cascade (.xml file) (Нагрузочный Каскад граней (.XML-файл))
     CascadeClassifier face_cascade;
     face_cascade.load("haarcascade_frontalface_alt2.xml");// Надо скопировать из opencv\build\etc\haarcascades\ в папку с проектом!!!
-​
-    if (!face_cascade.load("haarcascade_frontalface_alt2.xml")) { cout << "--(!)Error loading face cascade\n"; return -1; }; // Проверка загрузки каскада
+
+    // Проверка загрузки каскада
+    if (!face_cascade.load("haarcascade_frontalface_alt2.xml")) { cout << "--(!)Error loading face cascade\n"; return -1; }; 
 	
     // Детект лица
     vector<Rect> faces;
@@ -61,20 +34,8 @@ int main()
 			(чем выше , тем больше точность).
     		minSize=(20, 20) — Минимально возможный размер объекта. Объекты меньшего размера игнорируются. 
 	*/
-​
-    // Нарисовать круги на обнаруженных гранях. Можно нарисовать что угодно
-    for (int i = 0; i < faces.size(); i++)
-    {
-        Point center(faces[i].x + faces[i].width * 0.5, faces[i].y + faces[i].height * 0.5);
-        ellipse(image, center, Size(faces[i].width*0.5, faces[i].height*0.5), 0, 0, 360, Scalar(0, 0, 255), 1, LINE_AA, 0);
-    }
-    imshow("Detected Face", image);
-​
-    waitKey(0);
-    return 0;
-}
 
-    // Нарисовать круги на обнаруженных гранях
+    // Нарисовать круги на обнаруженных гранях. Можно нарисовать что угодно
     for (int i = 0; i < faces.size(); i++)
     {
         Point center(faces[i].x + faces[i].width * 0.5, faces[i].y + faces[i].height * 0.5);
